6dc33d710d9a13ae8ff259d7c28032cfd7d1ccbe
jwllamas <55216284+jwllamas@users.noreply.github.com>
2019-11-28 11:48:34 -0500

    added oauth pt 1

---
+++ api/v1/auth/callback.php
@@ -0,0 +1,93 @@
+<?php
+
+include("../../../provider.php");
+
+function print_r_results($accessToken, $curl, $endpoint)
+{
+    curl_setopt_array($curl, [
+        CURLOPT_URL => 'https://canvas.jmu.edu/api/v1' . $endpoint,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_HTTPHEADER => ['Authorization: Bearer ' . $accessToken]
+    ]);
+    // Send the request & save response to $myProfile
+    $response = curl_exec($curl);
+    if ($response === FALSE) {
+        die('Curl failed: ' . curl_error($curl));
+    }
+    return json_decode($response);
+}
+
+if (!isset($_GET[CODE])) {
+    $authorizationUrl = $provider->getAuthorizationUrl();
+    $_SESSION[STATE_LOCAL] = $provider->getState();
+    header("Location: $authorizationUrl");
+    exit;
+
+    /* check that the passed state matches the stored state to mitigate cross-site request forgery attacks */
+} elseif (empty($_GET[STATE]) || $_GET[STATE] !== $_SESSION[STATE_LOCAL]) {
+    unset($_SESSION[STATE_LOCAL]);
+    exit('Invalid state');
+} else {
+    /* try to get an access token (using our existing code) */
+    $token = $provider->getAccessToken('authorization_code', [CODE => $_GET[CODE]]);
+    $r_token = $token->getRefreshToken();
+    print_r($r_token, TRUE);
+    $_SESSION["access_token"] = $token->getToken();
+    $_SESSION["refresh_token"] = $r_token;
+
+    // print_r($_SESSION["access_token"]);
+    // print_r($_SESSION["refresh_token"]);
+    /* do something with that token... (probably not just print to screen, but whatevs...) */
+    // echo $token->getToken();
+
+    $curl = curl_init();
+    $profile = print_r_results($_SESSION["access_token"], $curl, '/users/self/profile');
+    $role = "";
+    $username = "";
+
+        // Check for the login id of the user, assign it to username
+        foreach ($profile as $k => $v) {
+            if ($k == 'login_id' ) {
+                $username = $v;
+            }
+        }
+
+        // Assign username to role
+        if ( $username == "llamasjw" ) {
+            $role = 'manager';
+
+        } else {
+            $role = 'student';
+        }
+
+        switch ($role) {
+            case 'student':
+                // redirect to student page
+                $_SESSION['role'] = 'student';
+                $_SESSION['username'] = $username;
+                header( "Location: http://www.google.com");
+                break;
+            case 'ta_reg':
+                 // redirect to regular ta page
+                 $_SESSION['role'] = 'ta';
+                 $_SESSION['username'] = $username;
+                header( "Location: http://www.google.com");
+                break;
+
+             case 'ta_lead':
+                 // redirect to ta lead page
+                 $_SESSION['role'] = 'ta_lead';
+                 $_SESSION['username'] = $username;
+                header( "Location: http://www.google.com");
+                break; 
+            case 'manager':
+                 // redirect to manager page
+                 $_SESSION['role'] = 'manager';
+                 $_SESSION['username'] = $username;
+                header( "Location: ../../../../home_manager.html");
+                break; 
+            }
+
+
+    exit;
+}
