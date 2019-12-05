867ad3a5c18afc9ed254790d15f9d001b54e83ed
Connor Wallace <connorjwallace@hotmail.com>
2019-12-04 22:20:23 -0500

    Update Calendar

--- calendar/calendar_script.php
+++ calendar/calendar_script.php
@@ -1,10 +1,8 @@
 <link href='./lib/fullcalendar.min.css' rel='stylesheet'/>
 <link href='./lib/fullcalendar.print.css' rel='stylesheet' media='print'/>
-<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <script src='./lib/jquery.min.js'></script>
 <script src='./lib/moment.min.js'></script>
 <script src='./lib/jquery-ui.custom.min.js'></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script src='./lib/fullcalendar.min.js'></script>
 
 <script>
@@ -23,51 +21,6 @@ var y = date.getFullYear();
 
 var calendar = $('#calendar').fullCalendar({
     editable: managerMode,
-    customButtons: {
-        add: {
-            text: 'add',
-            click: function() {
-                $('#add_dialog').dialog({
-                    open: function() {
-                        $('#shift_start').datepicker({title:'Test Dialog'}).blur();
-                        $('#shift_end').datepicker({title:'Test Dialog'}).blur();
-
-                    },
-                    close: function() {
-                        $('#shift_start').datepicker('destroy');
-                        $('#shift_end').datepicker('destroy');
-                    },
-
-                });
-                $( ".widget input[type=submit], .widget a, .widget button" ).button();
-                $( "button, input, a" ).click( function( event ) {
-                    event.preventDefault();
-                    var text = 
-                });
-                /*
-                if (title) {
-                    var end = fmt(end);
-                    $.ajax({
-                        url: 'add_shift.php',
-                        data: 'title=' + title + '&start=' + start + '&end=' + end,
-                        type: "POST",
-                        success: function (json) {
-                            alert('Added Successfully');
-                        }
-                    });
-                    calendar.fullCalendar('renderEvent',
-                        {
-                            title: title,
-                            start: start,
-                            end: end,
-                            allDay: allDay
-                        },
-                        true // make the event "stick"
-                    );
-                }*/
-            }
-        }
-    },
     header: {
         left: 'prev,next today add',
         center: 'title',
@@ -85,9 +38,8 @@ var calendar = $('#calendar').fullCalendar({
     },
         selectable: managerMode,
         selectHelper: true,
-    /*
         select: function (start, end, allDay) {
-        var title = prompt('Event Title:');
+        var title = prompt('Username:');
         if (title) {
             var start = fmt(start);
             var end = fmt(end);
@@ -111,8 +63,6 @@ var calendar = $('#calendar').fullCalendar({
           }
         calendar.fullCalendar('unselect');
     },
-    */
-
         editable: managerMode,
         eventDrop: function (event, delta) {
         var start = fmt(event.start);
